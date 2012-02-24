81f0e7a866b23cb9f592c30a84f90274fda041a9
azheng <azheng270@gmail.com>
2012-02-24 09:34:44 +0800

    del base-src

--- base-src/WebContent/resource/jsSrc/overWork/DataModel.js
+++
@@ -1,7 +0,0 @@
-/**
- * @class 项目-日报导出建模
- * @author haoxiaojie
- */
-function DataModel() {
-} // end_class
-
--- base-src/WebContent/resource/jsSrc/upload/DataModel.js
+++
@@ -1,6 +0,0 @@
-/**
- * @class 
- * @author haoxiaojie
- */
-function DataModel() {
-} // end_class
